cddf2d5e32a700eb384369afeff160cb9152d362
Danger Casey <caseysoftware@users.noreply.github.com>
2026-03-19 18:01:31 -0400

    change routing
    redirects back to where you were. This is a manual decision so not great but it works
    
    Resolves https://github.com/web2project/modules-notebook/issues/6

--- notebook/do_note_aed.php
+++ notebook/do_note_aed.php
@@ -9,10 +9,23 @@ $company_id = (int) w2PgetParam($_POST, 'note_company', 0);
 $project_id = (int) w2PgetParam($_POST, 'note_project', 0);
 $task_id = (int) w2PgetParam($_POST, 'note_task', 0);
 
-$extras = '&company_id=' . $company_id . '&project_id=' . $project_id . '&task_id=' . $task_id;
+$successRedirect = 'm=notebook';
+$extras = '';
+if ($company_id) {
+    $successRedirect = 'm=companies&a=view';
+    $extras = '&company_id=' . $company_id;
+}
+if ($project_id) {
+    $successRedirect = 'm=projects&a=view';
+    $extras = '&project_id=' . $project_id;
+}
+if ($task_id) {
+    $successRedirect = 'm=tasks&a=view';
+    $extras = '&task_id=' . $task_id;
+}
 
 $controller = new w2p_Controllers_Base(
-    new CNotebook(), $delete, 'Notebook', 'm=notebook', 'm=notebook&a=addedit' . $extras
+    new CNotebook(), $delete, 'Notebook', $successRedirect . $extras, 'm=notebook&a=addedit' . $extras
 );
 
 $AppUI = $controller->process($AppUI, $_POST);
